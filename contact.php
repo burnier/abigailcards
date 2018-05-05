9ca552f7b6a528489ef84edb40157e1ff45ad2c0
Daniel Burnier de Castro <burnier@gmail.com>
2018-05-05 15:39:06 +0200

    /de

---
+++ contact.php
@@ -0,0 +1,42 @@
+<?php
+
+// configure
+$from = 'Demo contact form <contact@abigailcards.com>';
+$sendTo = 'Demo contact form <contact@abigailcards.com>';
+$subject = 'New message from contact form';
+$fields = array('name' => 'Name', 'surname' => 'Surname', 'phone' => 'Phone', 'email' => 'Email', 'message' => 'Message'); // array variable name => Text to appear in email
+$okMessage = 'Formular erfolgreich versendet! Ich werde baldmöglichst mit Dir Kontakt aufnehmen.';
+$errorMessage = 'Ein Fehler ist aufgetreten. Versuche es bitte später erneut.';
+
+// let's do the sending
+
+try
+{
+    $emailText = "You have new message from contact form\n=============================\n";
+
+    foreach ($_POST as $key => $value) {
+
+        if (isset($fields[$key])) {
+            $emailText .= "$fields[$key]: $value\n";
+        }
+    }
+
+    mail($sendTo, $subject, $emailText, "From: " . $from);
+
+    $responseArray = array('type' => 'success', 'message' => $okMessage);
+}
+catch (\Exception $e)
+{
+    $responseArray = array('type' => 'danger', 'message' => $errorMessage);
+}
+
+if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+    $encoded = json_encode($responseArray);
+    
+    header('Content-Type: application/json');
+    
+    echo $encoded;
+}
+else {
+    echo $responseArray['message'];
+}
